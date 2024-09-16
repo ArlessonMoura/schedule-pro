ac2706fa7890edb5075787692124fbb48d780d5b
pedrohsouza <pedrosouza185@gmail.com>
2024-09-16 20:08:00 -0300

    💄 Improve Login layout

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,21 @@
 export default function Login() {
   return (
-    <>
-      <h1>Hello</h1>
-    </>
-  );
+    <div className="flex flex-col justify-center items-center h-screen bg-cover bg-[url('https://cdn.arstechnica.net/wp-content/uploads/2023/06/bliss-update-800x533.jpg')]">
+      <form className="rounded-3xl border-2 border-neutral-950 flex flex-col gap-4 py-16 px-16 bg-white max-w-md">
+        <input
+          className="rounded-lg border-2 border-neutral-950 py-2 placeholder:text-center px-4"
+          type="text"
+          placeholder="Name"
+        />
+        <input
+          className="rounded-lg border-2 border-neutral-950 py-2 placeholder:text-center px-4"
+          placeholder="Password"
+          type="password"
+          name=""
+          id=""
+        />
+        <button className="bg-green-600 py-2 rounded-lg">Enter</button>
+      </form>
+    </div>
+  )
 }

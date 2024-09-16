e55243a41dac4e897e42696541ebc512afdf51c9
pedrohsouza <pedrosouza185@gmail.com>
2024-09-16 19:12:49 -0300

    💄 Improve ProfileCard layout

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -1,21 +1,26 @@
-import { FaTrashAlt, FaPen } from 'react-icons/fa';
+import { FaTrashAlt, FaPen } from 'react-icons/fa'
 
 export default function ProfileCard(props: {
-  image: string;
-  textInfo: string;
+  image: string
+  textInfo: string
 }) {
-  const { image, textInfo } = props;
-  return (
-    <section className="grid grid-cols-12 grid-rows-6 gap-4 border-4 border-gray-400">
-      <img className="col-span-4 row-span-3" src={image} alt="profile" />
+  const { image, textInfo } = props
 
-      <article className="col-span-8 col-start-5 row-span-4">
+  return (
+    <section className="grid grid-cols-3 grid-rows-4 gap-4 border-2 border-gray-300 p-4">
+      <img
+        className="row-span-3"
+        src="https://reqres.in/img/faces/2-image.jpg"
+        alt="profile"
+      />
+
+      <article className="flex justify-between flex-col col-span-2 row-span-3">
         <p>Name: {textInfo}</p>
         <p>Last Name: {textInfo}</p>
         <p>Email: {textInfo}</p>
       </article>
 
-      <div className="flex justify-center col-span-8 col-start-5 row-span-2 row-start-5 gap-4">
+      <div className="flex justify-evenly col-span-2 col-start-2 row-start-4">
         <button>
           <FaTrashAlt />
         </button>
@@ -24,5 +29,5 @@ export default function ProfileCard(props: {
         </button>
       </div>
     </section>
-  );
+  )
 }

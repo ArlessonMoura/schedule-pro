69a88e62ee5d3a2b8e1a67fc7aabd4f93553116d
pedrohsouza <pedrosouza185@gmail.com>
2024-09-16 19:17:46 -0300

    💄 Improve ProfileList layout

--- src/pages/ProfileList.tsx
+++ src/pages/ProfileList.tsx
@@ -1,5 +1,5 @@
 // import { useEffect } from 'react';
-import ProfileCard from '../components/ProfileCard';
+import ProfileCard from '../components/ProfileCard'
 
 export default function ProfileList() {
   // useEffect(){
@@ -7,13 +7,19 @@ export default function ProfileList() {
   // }
 
   return (
-    <main className="grid grid-cols-2 gap-4">
-      <ProfileCard />
-      <ProfileCard />
-      <ProfileCard />
-      <ProfileCard />
-      <ProfileCard />
-      <ProfileCard />
-    </main>
-  );
+    <>
+      <header className="flex justify-between py-4 px-8">
+        <p>email</p>
+        <button className="bg-green-600 py-2 px-8 rounded-lg">+ Add</button>
+      </header>
+      <main className="grid md:grid-cols-2 gap-4 p-8">
+        <ProfileCard />
+        <ProfileCard />
+        <ProfileCard />
+        <ProfileCard />
+        <ProfileCard />
+        <ProfileCard />
+      </main>
+    </>
+  )
 }

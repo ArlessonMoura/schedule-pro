0ecd828f4c7487d508aa3a68657b72bb3af81eba
pedrohsouza <pedrosouza185@gmail.com>
2024-09-24 18:19:24 -0300

    ✨ Add animation to ProfileCard

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -1,4 +1,5 @@
 import { FaTrashAlt, FaPen } from 'react-icons/fa'
+import { animated, useSpring } from '@react-spring/web'
 
 type User = {
   id: string
@@ -11,24 +12,31 @@ type User = {
 export default function ProfileCard({ user }: { user: User }) {
   const { id, email, first_name, last_name, avatar } = user
 
+  const springs = useSpring({
+    from: { y: 100 },
+    to: { y: 0 }
+  })
+
   return (
-    <section className="grid grid-cols-3 grid-rows-4 gap-4 border-2 border-gray-300 p-4">
-      <img className="row-span-3" src={avatar} alt="profile" />
+    <animated.div style={{ ...springs }}>
+      <section className="grid grid-cols-3 grid-rows-4 gap-4 border-2 border-gray-300 p-4">
+        <img className="row-span-3" src={avatar} alt="profile" />
 
-      <article className="flex justify-between flex-col col-span-2 row-span-3">
-        <p>Name: {first_name}</p>
-        <p>Last Name: {last_name}</p>
-        <p>Email: {email}</p>
-      </article>
+        <article className="flex justify-between flex-col col-span-2 row-span-3">
+          <p>Name: {first_name}</p>
+          <p>Last Name: {last_name}</p>
+          <p>Email: {email}</p>
+        </article>
 
-      <div className="flex justify-evenly col-span-2 col-start-2 row-start-4">
-        <button>
-          <FaTrashAlt />
-        </button>
-        <button>
-          <FaPen />
-        </button>
-      </div>
-    </section>
+        <div className="flex justify-evenly col-span-2 col-start-2 row-start-4">
+          <button>
+            <FaTrashAlt />
+          </button>
+          <button>
+            <FaPen />
+          </button>
+        </div>
+      </section>
+    </animated.div>
   )
 }

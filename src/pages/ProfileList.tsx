e6eb40632f5bd7b120455fa53282b2338c74990f
pedrohsouza <pedrosouza185@gmail.com>
2024-09-27 11:58:54 -0300

    ♻️ Separate types in a  different file | add types to fetch function

--- src/pages/ProfileList.tsx
+++ src/pages/ProfileList.tsx
@@ -4,35 +4,33 @@ import React from 'react'
 import { useInView } from 'react-intersection-observer'
 import Header from '../components/Header'
 import { Toaster } from '@/components/ui/toaster'
+import { Page } from '@/types/custom'
 
 export default function ProfileList() {
   const { ref, inView } = useInView()
 
-  const fetchUsers = async ({ pageParam }) => {
+  const fetchUsers = async ({
+    pageParam
+  }: {
+    pageParam: number
+  }): Promise<Page> => {
     const res = await fetch('https://reqres.in/api/users?page=' + pageParam)
     return await res.json()
   }
 
-  const {
-    data,
-    error,
-    fetchNextPage,
-    hasNextPage,
-    isFetching,
-    isFetchingNextPage,
-    status
-  } = useInfiniteQuery({
-    queryKey: ['users'],
-    queryFn: fetchUsers,
-    initialPageParam: 1,
-    getNextPageParam: lastPage => {
-      const isFinalPage = lastPage.page === lastPage.total_pages
-      if (isFinalPage) {
-        return null
+  const { data, error, fetchNextPage, isFetching, isFetchingNextPage, status } =
+    useInfiniteQuery({
+      queryKey: ['users'],
+      queryFn: fetchUsers,
+      initialPageParam: 1,
+      getNextPageParam: lastPage => {
+        const isFinalPage = lastPage.page === lastPage.total_pages
+        if (isFinalPage) {
+          return null
+        }
+        return lastPage.page + 1
       }
-      return lastPage.page + 1
-    }
-  })
+    })
 
   React.useEffect(() => {
     if (inView) {
@@ -50,25 +48,13 @@ export default function ProfileList() {
       <main className="grid md:grid-cols-2 gap-4 p-8">
         {data.pages.map((page, i) => (
           <React.Fragment key={i}>
-            {page.data.map(
-              (user: {
-                id: string
-                email: string
-                first_name: string
-                last_name: string
-                avatar: string
-              }) => {
-                return <ProfileCard key={user.id} user={user} />
-              }
-            )}
+            {page.data.map(user => {
+              return <ProfileCard key={user.id} user={user} />
+            })}
           </React.Fragment>
         ))}
         <div ref={ref}>
-          {isFetchingNextPage
-            ? 'Loading more...'
-            : hasNextPage
-            ? 'Load Newer'
-            : 'Nothing more to load'}
+          {isFetchingNextPage ? 'Loading more...' : 'Nothing more to load'}
         </div>
         <div>
           {isFetching && !isFetchingNextPage ? 'Background Updating...' : null}

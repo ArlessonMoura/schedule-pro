6b36efdce8b4cb7a2ffa18bfeac2d1a0b6699fad
pedrohsouza <pedrosouza185@gmail.com>
2024-09-28 15:48:18 -0300

    ➕ Add react query devtools

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Login from './pages/Login'
 import ProfileList from './pages/ProfileList'
 import NotFound from './pages/NotFound'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 const queryClient = new QueryClient()
 
@@ -14,6 +15,7 @@ function App() {
         <Route path="/list" element={<ProfileList />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   )
 }
